478a75207e0bb792d36bb831e3dcb11df3dab1ac
Ed <125836816+EddyDev1@users.noreply.github.com>
2023-10-01 16:44:47 -0500

    Deleted major.h, only going to have files I had in my branch during development

--- major.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAJOR_H
-#define MAJOR_H
-
-unsigned int rotate_right(unsigned int input_number, int rotation_value);
-unsigned int endian(unsigned int input_number);
-unsigned long int parity(unsigned long int x);
-int count_leading_zeros(unsigned int num);
-
-#endif 
