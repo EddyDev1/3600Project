cb24243522f56b1a8591d0a44e36fde83ea2806c
Ed <125836816+EddyDev1@users.noreply.github.com>
2024-05-26 21:25:42 -0500

    simplified for readability
    regarding parity.c

--- parity.c
+++ parity.c
@@ -1,16 +1,19 @@
 #include "major.h"
 #include <stdio.h>
 unsigned long int parity(unsigned long int x){
-    while (x <0 || x > 4294967295){
+    while (x < 0 || x > 4294967295)
+    {
         printf("INVALID INPUT: You entered an invalid input for the parity value\n");
         printf("Enter a 32-bit number (>= 1 and <= 4294967295, inclusively): ");
-        scanf("%ld",&x);
+        scanf("%ld", &x);
     }
-  int c = 0;
-  for(int i = 7, j = 0; i>=0; i--, j++){
-      unsigned long int hold = x>>i;
-      if(hold&1) c++;
+    
+    int c = 0;
+    for(int i = 7, j = 0; i >= 0; i--, j++)
+    {
+        unsigned long int hold = x >> i;
+        c = hold & 1 ? ++c : c;
     }
-  if(c%2 == 0) return 0;
-  return 1;
+
+    return c % 2 ? 0 : 1;
 }
